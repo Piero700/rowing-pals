49eab423be5c9aaa83815942c514c517931af2d9
Piero Ciobanu <ciobanupiero5@gmail.com>
2026-09-08 22:32:15 +0100

    Initial Commit

---
+++ Rowing Pals/Rowing_PalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rowing_PalsApp.swift
+//  Rowing Pals
+//
+//  Created by Piero on 08/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Rowing_PalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rowing PalsTests/Rowing_PalsTests.swift
@@ -0,0 +1,19 @@
+//
+//  Rowing_PalsTests.swift
+//  Rowing PalsTests
+//
+//  Created by Piero on 08/09/2026.
+//
+
+import Testing
+@testable import Rowing_Pals
+
+struct Rowing_PalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
